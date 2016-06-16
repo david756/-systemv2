44b25fea1a65c5e4cc21aa89cad98d7f0c157aec
david756 <pipehernandez1020@hotmail.com>
2016-06-15 19:56:56 -0500

    update

--- Version 1.0/V1post/includes/db/serv.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-    } 
-	$conect = @mysql_connect("localhost","root","") or die("No se encontró el servidor");
-	mysql_select_db("cafe_superior",$conect)or die("No se encontró la base de datos");
-?>
---
+++ lib/model/Inventario.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Inventario
+ *
+ * @author David
+ */
+class Inventario {
+    //put your code here
+}
