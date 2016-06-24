80ef2c4ac7ed2cefa14e8a74550cae0c2e9434e5
david756 <pipehernandez1020@hotmail.com>
2016-06-23 19:49:08 -0500

    Vista Pedidos_producto terminada, tambien pedido_mesas y login

---
+++ lib/footer.php
@@ -0,0 +1,9 @@
+        <!-- footer content -->
+        <footer>
+          <div class="copyright-info">
+            <p class="pull-right">Mantil - Template by Colorlib</a>
+            </p>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
